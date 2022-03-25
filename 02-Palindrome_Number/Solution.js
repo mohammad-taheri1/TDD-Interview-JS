bfe50bba8137fea7cec5dcb8d921958580fa0eae
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-25 11:17:53 +0430

    update 02-Palindrome_Number

--- 02-Palindrome_Number/Solution.js
+++ 02-Palindrome_Number/Solution.js
@@ -1,9 +1,28 @@
+// ***************  solution1  ***************
 /**
  * @param {number} inputNumber
  * @return {boolean}
  */
-const isPalindrome = (inputNumber) => {
-    return parseInt(inputNumber) === parseInt(inputNumber.toString().split('').reverse().join(''));
+// const isPalindrome = (inputNumber) => {
+//     return parseInt(inputNumber) === parseInt(inputNumber.toString().split('').reverse().join(''));
+
+// }
+
+// ***************  solution2  ***************
+/**
+ * @param {number} inputNumber
+ * @return {boolean}
+ */
+ const isPalindrome = (inputNumber) => {
+        if(inputNumber < 0) return false;
+        let reversedInput = 0;
+
+        for(let i=inputNumber; i>=1; i = Math.floor(i/10)){
+            reversedInput = reversedInput * 10 + i%10;
+        }
+
+        return reversedInput === inputNumber;
+
 }
 
 module.exports = isPalindrome;
