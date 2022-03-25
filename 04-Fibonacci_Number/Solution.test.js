308f19b40986290ab4cbfc510e6b89d214510717
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-25 16:20:15 +0430

    update

--- 04-Fibonacci_Number/Solution.test.js
+++ 04-Fibonacci_Number/Solution.test.js
@@ -2,6 +2,39 @@ const fibonacci = require('./Solution');
 
 describe('check fibonacci', () => {
 
+    it('should return 0 for input of 0 ', () => {
+        expect(fibonacci(0)).toBe(0);
+    });
+    it('should return 1 for input of 1 ', () => {
+        expect(fibonacci(1)).toBe(1);
+    });
+    it('should return 1 for input of 2 ', () => {
+        expect(fibonacci(2)).toBe(1);
+    });
+    it('should return 2 for input of 3 ', () => {
+        expect(fibonacci(3)).toBe(2);
+    });
+    it('should return 3 for input of 4 ', () => {
+        expect(fibonacci(4)).toBe(3);
+    });
+    it('should return 5 for input of 5 ', () => {
+        expect(fibonacci(5)).toBe(5);
+    });
+    it('should return 8 for input of 6 ', () => {
+        expect(fibonacci(6)).toBe(8);
+    });
+    it('should return 13 for input of 7 ', () => {
+        expect(fibonacci(7)).toBe(13);
+    });
+    it('should return 21 for input of 8 ', () => {
+        expect(fibonacci(8)).toBe(21);
+    });
+    it('should return 34 for input of 9 ', () => {
+        expect(fibonacci(9)).toBe(34);
+    });
+    it('should return 55 for input of 10 ', () => {
+        expect(fibonacci(10)).toBe(55);
+    });
     
  
 });
--- README.md
+++ README.md
@@ -1,9 +1,36 @@
-# TDD-Interview-JS
+# Interview Tasks with TDD approach using JavaScript language and Jest library 
 
+## How to download & config:
+
+In order to use this repo:
+
+1- clone it using this command:
+
+    git clone https://github.com/MamadTaheri68/TDD-Interview-JS.git
+
+2-run this command inside `TDD-Interview-JS` folder:
+
+     npm install
+
+## How to use ?
+
+To run normal test: 
+
+    npm run test
+
+To run live test and check the result real-time: 
+
+    npm run liveTest
+
+To run comprehensive test with additional Information:
+
+    npm run fullTest
+
+## Contents:
     01-Valid Parentheses
 
     02-Palindrome Number
 
     03-Palindrome String
 
-    04-Fibonacci_Number
+    04-Fibonacci Number
