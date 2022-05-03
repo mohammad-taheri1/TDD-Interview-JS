0b6e99d7db93055e58550d955d45fc47f990a0a7
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-03 18:59:22 +0430

    update s004

--- 004-reverse-string/README.md
+++ 004-reverse-string/README.md
@@ -1,5 +1,5 @@
 ## Task 004: 004-reverse-string
 
-*  [linkedin video]()
+*  [linkedin video](https://www.linkedin.com/posts/mohammad-taheri1_tdd-javascript-jest-activity-6927262347858231296-ffdn?utm_source=linkedin_share&utm_medium=member_desktop_web)
 
-*  [youtube video]()
+*  [youtube video](https://www.youtube.com/watch?v=prC7M7wUKg4)
--- 004-reverse-string/reverseString.js
+++ 004-reverse-string/reverseString.js
@@ -1,5 +1,31 @@
+// # 1
+// function reverseString(inputString) {
+//   let temp = "";
+
+//   for(let i = inputString.length - 1; i >= 0; i--) {
+//     temp += inputString[i];
+//   }
+
+//   return temp;
+// }
+
+// # 2
+// function reverseString(inputString) {
+//   let temp = inputString.split('');
+//   temp = temp.reverse();
+//   return temp.join('');
+// }
+
+// # 3
 function reverseString(inputString) {
-  //
+  let temp = "";
+
+  for (let char of inputString) {
+    temp = char + temp; 
+  }
+
+  return temp;
 }
+
 
 module.exports = reverseString;
