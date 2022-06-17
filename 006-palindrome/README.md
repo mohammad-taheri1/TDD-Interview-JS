e7811fea47010b93557a13c2ed8fc046a1306ade
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-06-17 19:22:12 +0430

    update s06 - palindrome

--- 006-palindrome/README.md
+++ 006-palindrome/README.md
@@ -1,5 +1,5 @@
 ## Task 006: 006-palindrome
 
-*  [linkedin video]( )
+*  [linkedin video](https://www.linkedin.com/posts/mohammad-taheri1_tdd-javascript-jest-activity-6943574768415592449-y9vX?utm_source=linkedin_share&utm_medium=member_desktop_web)
 
-*  [youtube video]()
+*  [youtube video](https://www.youtube.com/watch?v=uLPbaEm2g6Y)
--- 006-palindrome/palindrome.js
+++ 006-palindrome/palindrome.js
@@ -1,5 +1,31 @@
-function palindrome(str){
-    //
+// Solution 1
+// function palindrome(inputString){
+//     //step 1 : reverse our input string
+//     let reversedString = inputString.split('').reverse().join('');
+
+//     // step 2: check original and reverted string and return appropriate answer
+
+//     if(inputString.toLowerCase() === reversedString.toLowerCase()) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// Solution 2
+// function palindrome(inputString){
+//     //step 1 : reverse out input string
+//     let reversedString = inputString.split('').reverse().join('');
+
+//     // step 2: check original and reverted string and return appropriate answer
+
+//     return (inputString.toLowerCase() === reversedString.toLowerCase()) ? true : false;
+// }
+
+// Solution 3
+function palindrome(inputString){
+
+    return (inputString.toLowerCase() === inputString.split('').reverse().join('').toLowerCase()) ? true : false;
 }
 
 module.exports = palindrome;
