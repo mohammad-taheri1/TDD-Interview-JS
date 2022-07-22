39424e3db5e17c0d493b2de23985cacc9b67f14d
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-07-22 14:20:16 +0430

    update palindromeInteger

--- 007-palindrome-integeer/palindromeInteger.js
+++ 007-palindrome-integeer/palindromeInteger.js
@@ -1,5 +1,11 @@
 function palindromeInteger(inputNumber) {
-  //
+  // Step1: handle zero input
+
+  // Step2: reverse input number
+
+  // Step 3: return appropriate boolean value
+  // Dont forget the sign of input number
+  
 }
 
 module.exports = palindromeInteger;
