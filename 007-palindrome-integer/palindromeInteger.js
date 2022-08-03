fea40633cb5edb084ca540d6610137a7529c8185
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-08-04 01:38:29 +0430

    add another solution to palindromeInteger

--- 007-palindrome-integer/palindromeInteger.js
+++ 007-palindrome-integer/palindromeInteger.js
@@ -1,23 +1,42 @@
+// Solution One -> with string convert and array helper functions
+// function palindromeInteger(inputNumber) {
+//   // Step1: handle zero input
+//   if(inputNumber === 0) return false;
+
+//   // Step2: reverse input number
+//   const reversedInput = inputNumber
+//       .toString()
+//       .split('')
+//       .reverse()
+//       .join('');
+
+//   // Step 3: return appropriate boolean value
+//   // Dont forget the sign of input number
+//   if( parseInt(inputNumber) === Math.sign(inputNumber) * parseInt(reversedInput)) {
+//     return true;
+//   }
+//   return false;
+
+// }
+
+// Solution two -> just numbers operations
 function palindromeInteger(inputNumber) {
-  // Step1: handle zero input
-  if(inputNumber === 0) return false;
+  if (inputNumber === 0) return false;
 
+  let reversedNumber = 0;
+  let originalNumber = inputNumber;
 
-  // Step2: reverse input number
-  const reversedInput = inputNumber
-      .toString()
-      .split('')
-      .reverse()
-      .join('');
-
-      
-  // Step 3: return appropriate boolean value
-  // Dont forget the sign of input number
-  if( parseInt(inputNumber) === Math.sign(inputNumber) * parseInt(reversedInput)) {
-    return true;
+  if (inputNumber < 0) {
+    originalNumber *= -1;
   }
-  return false;
-  
+
+  while (originalNumber > 0) {
+    reversedNumber = reversedNumber * 10 + (originalNumber % 10);
+    originalNumber = Math.floor(originalNumber / 10);
+    console.log(`originalNumber: ${originalNumber} - rev :  ${reversedNumber}`);
+  }
+
+  return reversedNumber * Math.sign(inputNumber) === inputNumber;
 }
 
 module.exports = palindromeInteger;
