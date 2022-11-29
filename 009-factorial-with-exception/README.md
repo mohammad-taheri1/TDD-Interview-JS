2f0101e4f74afb9301f5a90c03196d7a44da60b4
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-11-29 21:13:33 +0330

    update s009

--- 009-factorial-with-exception/README.md
+++ 009-factorial-with-exception/README.md
@@ -2,4 +2,4 @@
 
 ### Implementing Factorial with exception in JavaScript using TDD (Test Driven Developement)
 
-*  [youtube video]()
+*  [youtube video](https://www.youtube.com/watch?v=BJ2X98LbduY)
--- 009-factorial-with-exception/factorialWithException.js
+++ 009-factorial-with-exception/factorialWithException.js
@@ -1,5 +1,36 @@
+// Solution One
+// function factorialWithException(inputNumber) {
+//   // negative check
+//   if (inputNumber < 0) throw new Error("negative");
+
+//   // zero input
+//   if (inputNumber === 0) throw new Error("zero");
+
+//   let result = 1;
+
+//   for (let i = inputNumber; i >= 1; i--) {
+//     result *= i;
+//   }
+
+//   return result;
+// }
+
+// Solution Two
 function factorialWithException(inputNumber) {
-  //
+  // negative check
+  if (inputNumber < 0) throw new Error("negative");
+
+  // zero input
+  if (inputNumber === 0) throw new Error("zero");
+
+  if(inputNumber === 1) {
+    return 1;
+  }
+
+  // 5! = 5 * 4!
+  return inputNumber * factorialWithException(inputNumber - 1)
+
+ 
 }
 
 module.exports = factorialWithException;
